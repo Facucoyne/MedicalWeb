82a99a502f5d98c8a56814fd6ea202d13c054e95
Facucoyne <hunter.bl12@yahoo.com>
2025-09-01 19:10:20 -0300

    newColors

--- src/assets/components/pages/Planes.jsx
+++ src/assets/components/pages/Planes.jsx
@@ -8,13 +8,13 @@ export const Planes = () => {
         <img src={Img1} alt="" />
         <h2 className='ConsultasText'>Consultas Medicas</h2>
   <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Correo</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <label for="exampleInputEmail1" className="form-label" >Correo</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Escribe su correo'></input>
         <p className='text'>Esta funcion se encuentra en desarrollo</p>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"></input>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Escribe una contraseña'></input>
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
